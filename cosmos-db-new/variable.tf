1b2aaa083530a6f5b532fc5acf648ccb0cf3ccd5
raghava0103 <121283372+raghava0103@users.noreply.github.com>
2023-01-02 18:01:30 +0530

    Update variable.tf

--- cosmos-db-new/variable.tf
+++ cosmos-db-new/variable.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" {
-  default = "testaks"
+  default = "RG-CLIENT-TEST-POC"
 }
 variable "resource_group_location" {
   default = "East US"
 }
 variable "subscription_id" {
-  default = "5e033ca6-1e01-4dca-b6d5-4e779d0e0aeb"
+  default = "ceaa9e66-993b-4b12-b3c2-e13d7ec61e71"
 }
 variable "tenant_id" {
   default = "87e35bb7-0b2f-43cc-94e0-65248787e14c"
