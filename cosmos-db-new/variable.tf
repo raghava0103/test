8c610fc74a84525ecacc2770143be7d6d30712f3
raghava0103 <121283372+raghava0103@users.noreply.github.com>
2023-01-02 15:29:07 +0530

    Create variable.tf

---
+++ cosmos-db-new/variable.tf
@@ -0,0 +1,18 @@
+variable "resource_group_name" {
+  default = "testaks"
+}
+variable "resource_group_location" {
+  default = "East US"
+}
+variable "subscription_id" {
+  default = "5e033ca6-1e01-4dca-b6d5-4e779d0e0aeb"
+}
+variable "tenant_id" {
+  default = "87e35bb7-0b2f-43cc-94e0-65248787e14c"
+}
+variable "cosmos_db_account_name" {
+  default = "test-cb"
+}
+variable "failover_location" {
+  default = "East US"
+}
