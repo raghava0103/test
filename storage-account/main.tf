40a2eb8bd1dd165166259f6c8eddba00f7f6e45d
raghava0103 <121283372+raghava0103@users.noreply.github.com>
2023-01-02 16:11:11 +0530

    Create main.tf

---
+++ storage-account/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.93.0"
+    }
+    azuread = {
+      source = "hashicorp/azuread"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+resource "azurerm_storage_account" "lab" {
+  name                     = "stoargetest1234"
+  resource_group_name      = "testaks"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "Terraform Storage"
+    CreatedBy   = "Admin"
+  }
