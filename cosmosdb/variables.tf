10287651b324cc85386c99944b8ac676945ab17b
raghava0103 <121283372+raghava0103@users.noreply.github.com>
2023-01-02 18:43:21 +0530

    Update variables.tf

--- cosmosdb/variables.tf
+++ cosmosdb/variables.tf
@@ -1,17 +1,11 @@
 variable "resource_group_name" {
-  default = "testaks"
+  default = "RG-CLIENT-TEST-POC"
 }
 variable "resource_group_location" {
   default = "East US"
 }
-variable "subscription_id" {
-  default = "5e033ca6-1e01-4dca-b6d5-4e779d0e0aeb"
-}
-variable "tenant_id" {
-  default = "87e35bb7-0b2f-43cc-94e0-65248787e14c"
-}
 variable "cosmos_db_account_name" {
-  default = "test-cb"
+  default = "ascendion-db"
 }
 variable "failover_location" {
   default = "East US 2"
